faa76da2c07b9c99d1ad216353eaa942fa55d232
hatka_bobrikov <rodionov.artem62@mail.ru>
2023-10-10 13:09:47 +0300

    delete Main.java from src

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author hatka_bobrikov
- * @created 6.10.2023 15:33
- */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
